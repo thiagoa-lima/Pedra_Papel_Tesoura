c2e0ea085ba37fc3c35564195d9dc61046ee7bae
thiagoandradelima <thiagog_12@hotmail.com>
2023-01-10 15:13:02 -0300

    Condicionais IF
    Inlcusão das condicionais If para inicio do jogo

--- jogo.py
+++ jogo.py
@@ -1,11 +1,8 @@
-
+import time
 def play_game():
 
     msg_abertura()
     comece_o_jogo()
-
-
-
 
 def msg_abertura():
     print("=" * 41)
@@ -25,16 +22,26 @@ def comece_o_jogo():
 
         escolha = int(input("\nQual você escolhe? "))
 
+        if escolha not in (1, 2, 3):
+            print("Opção inválida. Vamos começar novamente.")
+            continue
+
+        print("JO...")
+        time.sleep(.5)
+        print("KEN...")
+        time.sleep(.5)
+        print("PÔ...")
+        time.sleep(.5)
+        print("-" * 30)
+
         if escolha == 1:
             print("Você escolheu PEDRA.")
         elif escolha == 2:
             print("Você escolheu PAPEL.")
         elif escolha == 3:
             print("Você escolheou TESOURA.")
-        else:
-            print("Opção inválida. Vamos começar novamente.")
-            continue
 
+        print("-" * 30)
 
 
 
